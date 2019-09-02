c0b3ef5999b5c273668a9180d2fbb6c06c06d564
Peter Ayeni <ayenicology@gmail.com>
2019-09-02 18:48:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -41,7 +41,7 @@ def third_challenge
         },
       :heroine_friends => [
         {:name => "Steven", :age => "30", :attitude => "confused"},
-        {:name => "Nurse", :age => "44", :attitude => "worried"}
+        { name: "Nurse", :age => "44", :attitude => "worried"}
         ]
    }
   }
